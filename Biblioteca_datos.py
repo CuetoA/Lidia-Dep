13dab1c49facf881dbd1f294f03d1c08a54a8e47
Andres Cueto <andrescuetoestrada@gmail.com>
2021-02-13 13:11:24 -0600

    Será una de las primeras funciones en ejecutarse y deberá devolver objetos con las características requeridas para su ejecución en main

---
+++ Biblioteca_datos.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 13 13:02:26 2021
+
+@author: Andrés Cueto Estrada
+Nombre: Biblioteca_datos
+Descripción: 
+    En este script se encuentran todos los datos requeridos para funcionar, tales como
+    la carpeta raiz
+
+    
+    
+Entradas: Ninguna
+Salidas: Objetos
+Comentarios: 
+"""
+
